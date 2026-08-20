5b16f519df78028318372f50b4e00f9708193f9b
kripa_26 <kripachhajer26@gmail.com>
2026-08-20 22:05:11 +0530

    day 2:add c++ compilation with g++ via exec , handel success/error cases

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,29 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const { exec } = require('child_process');
+
 app.use(cors());
 app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Server is working!');
 });
+
+app.post('/compile', (req, res) => {
+  const code = req.body.code;
+  fs.writeFileSync('temp.cpp', code);
+
+  exec('g++ temp.cpp -o temp.exe', (error, stdout, stderr) => {
+    if (error) {
+      res.send({ success: false, error: stderr });
+    } else {
+      res.send({ success: true, message: 'Compiled successfully!' });
+    }
+  });
+});
+
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
---
+++ backend/temp.cpp
@@ -0,0 +1,1 @@
+int main() { this is broken code }
