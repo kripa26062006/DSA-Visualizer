bc27597d28bd399762f9635a11f2811728843fb8
kripa_26 <kripachhajer26@gmail.com>
2026-08-23 22:53:57 +0530

    Day 4: Add code instrumentation for variable state tracking

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,23 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const { exec } = require('child_process');
 console.log('File started running');
+function instrumentCode(code){
+  const lines =code.split('\n');
+  let trackedVars =[];
+  let output =[];
+  for (let line of lines){
+  output.push(line);
+   const match = line.match(/^\s*(int|double|float|long long)\s+(\w+)\s*=/);
+    if (match){
+      const varName = match[2];
+      trackedVars.push(varName);
+
+      const printParts = trackedVars.map(v => `"${v}=" << ${v}`).join(' << "," << ');
+      output.push(`cout << "STEP|" << ${printParts} << endl;`);
+    }
+  }
+       return output.join('\n');
+}
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -14,8 +31,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/compile', (req, res) => {
-  const code = req.body.code;
-  fs.writeFileSync('temp.cpp', code);
+const code = req.body.code;
+const instrumentedCode = instrumentCode(code);
+fs.writeFileSync('temp.cpp', instrumentedCode);
 
   exec('g++ temp.cpp -o temp.exe', (error, stdout, stderr) => {
     if (error) {
--- backend/temp.cpp
+++ backend/temp.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
-int main() { cout << "Hello from CPP"; return 0; }
+int main() {
+int x = 5;
+cout << "STEP|" << "x=" << x << endl;
+int y = 10;
+cout << "STEP|" << "x=" << x << "," << "y=" << y << endl;
+int sum = x + y;
+cout << "STEP|" << "x=" << x << "," << "y=" << y << "," << "sum=" << sum << endl;
+return 0;
+}
